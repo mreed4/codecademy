a9f8dac6b56b819d8330e3710edf61576f9508e0
Matthew <78713317+mreed4@users.noreply.github.com>
2021-10-04 08:38:09 -0700

    Begin writing/ideating .mutilate() method

--- mysteriousOrganism.js
+++ mysteriousOrganism.js
@@ -13,4 +13,51 @@ const mockUpStrand = () => {
     return newStrand.join('');
 }
 
-console.log(mockUpStrand());
+let test = mockUpStrand();
+console.log(test);
+
+// Begin code
+const pAequorFactory = (n = 1, base) => {
+    return {
+        specimenNum: n,
+        dna: base,
+        mutate() {
+            let randBase = Math.floor(Math.random() * base.length);
+            let toChange = this.dna[randBase];
+            /*
+            for (let i = 0; i < base.length; i++) {
+                let toChange = this.dna[i];
+
+                let b = Math.floor(Math.random() * 3)
+                let swapBase = {
+                    a: {
+                        1: 'T',
+                        2: 'C',
+                        3: 'G'
+                    },
+                    t: {
+                        1: 'A',
+                        2: 'C',
+                        3: 'G'
+                    },
+                    c: {
+                        1: 'A',
+                        2: 'T',
+                        3: 'G'
+                    },
+                    g: {
+                        1: 'A',
+                        2: 'T',
+                        3: 'C'
+                    }
+                }
+                toChange = swapBase[toChange][b];
+
+            }
+            */
+            return `${toChange} i is ${randBase} `;
+        }
+    }
+}
+
+console.log(pAequorFactory(1, test).mutate());
